3a15d7fbad57940f134be00233bb3369f65cf786
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2017-10-17 14:56:26 +0300

    refactored a few times, added local storage, some more refining is needed for current day handling

--- app.js
+++ app.js
@@ -1,97 +1,83 @@
-Vue.component('tags-grid', {
-  template: '#grid-template',
-  replace: true,
-  props: {
-    data: Array,
-    columns: Array,
-    filterKey: String
-  },
-  data: function () {
-    var sortOrders = {}
-    this.columns.forEach(function (key) {
-      sortOrders[key] = 1
+const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+const STORAGE_KEY = 'day-one-goals-v1.0'
+
+function makeTodayGoal(new_id) {
+  var today = {
+    id: new_id,
+    goal: 'new',
+    streak: 0,
+    started: new Date(),
+  }
+  return today
+}
+function dateReviver(value) {
+  var isDate = false;
+    if (typeof value === "string" && dateFormat.test(value)) {
+      isDate = true
+        return [isDate, new Date(value)];
+    }
+    
+    return [false, value];
+}
+var goalStorage = {
+  fetch: function () {
+    var goals = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    console.log( 'attempt from localStorage' )
+    goals.forEach(function (goal, index) {
+      console.log( goal )
+      var isDate = dateReviver(goal.started)
+      if (isDate[0]) {
+        goal.started = isDate[1];
+      }
+
     })
-    return {
-      sortKey: '',
-      sortOrders: sortOrders
-    }
+
+    goals.push( makeTodayGoal(goals.length) )
+    goalStorage.next_id = goals.length
+
+    return goals
   },
-  computed: {
-    filteredData: function () {
-      var sortKey = this.sortKey
-      var filterKey = this.filterKey && this.filterKey.toLowerCase()
-      var order = this.sortOrders[sortKey] || 1
-      var data = this.data
-      if (filterKey) {
-        data = data.filter(function (row) {
-          return Object.keys(row).some(function (key) {
-            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
-          })
-        })
-      }
-      if (sortKey) {
-        data = data.slice().sort(function (a, b) {
-          a = a[sortKey]
-          b = b[sortKey]
-          return (a === b ? 0 : a > b ? 1 : -1) * order
-        })
-      }
-      return data
-    }
+  save: function (goals) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(goals))
+  }
+}
+
+var anApplication = new Vue({
+  el: '#application',
+  data: {
+    showColumns: ['goal', 'streak', 'started'],
+    tagData: goalStorage.fetch(),
+    selected: {
+      id : '',
+      goal: '',
+      streak: '',
+      started: '',
+    },
+  },
+  created: function() {
   },
   methods: {
-    sortBy: function (key) {
-      this.sortKey = key
-      this.sortOrders[key] = this.sortOrders[key] * -1
+    onRowClick: function(ev) {
+      var id = ev.currentTarget.id;
+      this.selected.id = id;
+      this.selected.goal = this.tagData[id].goal;
+      this.selected.streak = this.tagData[id].streak;
+      this.selected.started = this.tagData[id].started;
     },
-    tryMe: function(ty) {
-      alert(ty);
-    }
+    onCheckClick : function (event) {
+      this.selected.streak += 1
+      this.tagData[this.selected.id].streak = this.selected.streak
+      goalStorage.save( this.tagData )
+    },
+    prettyDate: function (str) {
+      var options = { month : "long", day : "numeric" };
+      var result = str.toLocaleDateString( 'en-GB', options );
+      return result;
+    },
   },
   filters: {
     capitalize: function (str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
     },
-    enrich: function(str) {
-      /*****/
-      var isNumeric = function(input) {
-          return (input - 0) == input && (''+input).trim().length > 0;
-      };
-      var isDate = function(input) {
-        dd = Date.parse( input );
-        //alert( "a " + input + " is " + (dd > 0) );
-        return (dd > 0);
-      };
-      /*****/
-      var prefix = '#'
-      var suffix = ''
-
-      if (isNumeric(str)) {
-        prefix = '< ';
-        suffix = ' >';
-      }
-      else if (isDate(str)) {
-        prefix = '{ ';
-        suffix = ' }'
-      }
-
-      return prefix+str+suffix;
-    },
   },
-})
-
-
-
-var anApplication = new Vue({
-  el: '#application',
-  data: {
-    searchQuery: '',
-    tagColumns: ['goal', 'streak', 'date'],
-    tagData: [
-      { goal: 'pushups', streak: 9, date: 'Oct-1' },
-      { goal: 'running', streak: 3, date: 'Oct-12' },
-      { goal: 'meditation', streak: 9, date: 'Oct-1' },
-    ]
-  }
-
 })
